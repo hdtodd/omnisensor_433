f1990fb3b9cdbd95cf594606ca5c24b9f9a626ac
H. David Todd (Mac) <hdtodd@gmail.com>
2025-05-26 15:36:45 -0400

    Fix Omni00 help

--- Omni00/Omni00AP.py
+++ Omni00/Omni00AP.py
@@ -1,9 +1,9 @@
 AP_NAME = "Omni00"
 AP_VERSION = "1.0"
 AP_DESCRIPTION="""
-IniCli:  Demonstration program to extract data from Omni Multisensor
-         protocol type 01 messages that have been transmitted as
-         protocol type 00 hexadecimal messages
+Omni00::  Demonstration program to extract data from Omni Multisensor
+          protocol type 01 messages that have been transmitted as
+          protocol type 00 hexadecimal messages
 """
 AP_EPILOG="""
 """
